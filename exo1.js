4f985ac0074b69c1b2c14a78bd92b49c57438cc9
ArthurMovellan <arthur.movellan@gmail.com>
2021-02-22 15:16:50 +0100

    premier commit  Please enter the commit message for your changes. Lines starting

---
+++ exo1.js
@@ -0,0 +1,9 @@
+console.log("Veuillez saisir une chaîne");
+let c1 =scanf('%s');
+console.log("Veuillez saisir la seconde chaîne");
+let c2 = scanf('%s');
+if(c1.length >= 2*c2.length){
+    console.log("la première chaîne est deux fois plus longue que la deuxième");
+}else{
+    console.log("la première chaîne n'est pas deux fois plus longue que la deuxième");
+}
